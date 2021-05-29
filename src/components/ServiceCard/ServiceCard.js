349f16cb21d3fdcc388649cfee74cfb20727b75d
misbahs100 <misbahuddins100@gmail.com>
2021-05-29 13:15:25 +0600

    testing commit

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -6,8 +6,8 @@ const ServiceCard = ({service}) => {
         <div className="col-md-4 d-flex justify-content-center">
             <div className="card p-5 text-center">
                 <h4>{service.title}</h4>
-                <p>{service.desc}</p>
-                <button className="btn btn-primary">Try This</button>
+                <p>{service.desc}</p> 
+                <button className="btn btn-primary">Try This </button>
             </div>
         </div>
     );
