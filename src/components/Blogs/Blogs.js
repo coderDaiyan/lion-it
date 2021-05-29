829e6713db95d01e495843b7327d3669fde76435
Abu Naser Dipu <abunaserdipu0@gmail.com>
2021-05-29 14:26:07 +0600

    blogs and footer launch

---
+++ src/components/Blogs/Blogs.js
@@ -0,0 +1,66 @@
+import React from "react";
+import passwordSafe from "../../images/password-safe.jpg";
+import httpOrHttps from "../../images/http-or-https.png";
+import cloudComputing from "../../images/cloud-computing.png";
+
+const Blogs = () => {
+  return (
+    <section>
+      <div className="container my-5">
+        <h2 className="text-brand text-center my-5">
+          You can explore your knowledge <br /> with our awesome blogs
+        </h2>
+        <div className="row">
+          <div className="col-md-4">
+            <div className=" shadow p-3 mb-5 bg-white rounded">
+              <img style={{ width: "100%" }} src={passwordSafe} alt="" />
+              <h4 className="text-brand my-3">
+                Ways to keep your password safe
+              </h4>
+              <p>
+                You must be having some basic knowledge on how, you can keep
+                your password safe, secure and protected. Like, you must keep a
+                long password with the maximum number of characters, using
+                numbers and symbols etc. Still, there are some common pitfalls,
+                that anybody with passwords should avoid keeping their password
+                safe.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className=" shadow p-3 mb-5 bg-white rounded">
+              <img style={{ width: "100%" }} src={httpOrHttps} alt="" />
+              <h4 className="text-brand my-3">
+                What you need to use http or https
+              </h4>
+              <p>
+                The HTTPS is better than HTTP because it provides security.
+                Sometimes our website does not contain an e-commerce page that
+                requires sensitive data; in that case, we can switch to the HTTP
+                protocol. Despite the security, HTTPS also provides SEO. So, we
+                do need to put more effort into boosting our SEO.
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="shadow p-3 mb-5 bg-white rounded">
+              <img style={{ width: "100%" }} src={cloudComputing} alt="" />
+              <h4 className="text-brand my-3">cloud computing advantages</h4>
+              <p>
+                Cloud computing operates on a similar principle as web-based
+                email clients, allowing users to access all of the features and
+                files of the system without having to keep the bulk of that
+                system on their own computers. Most people already use a variety
+                of cloud computing services without even realizing it. Gmail,
+                Google Drive, and even Facebook and Instagram are all
+                cloud-based applications.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Blogs;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Footer.css";
+// import lionItIcon from '../../images/footer';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faPhoneAlt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faInstagramSquare,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <section className="footer-container">
+      <div className="text-center my-5 py-5">
+        <h1 className="text-white">
+          {/* <img
+            style={{ width: "100px", color: "white" }}
+            src={repairIcon}
+            alt=""
+          /> */}
+          Lion IT Solution
+        </h1>
+        <h5 className="text-brand">ADDRESS:</h5>
+        <p className="text-white">Rotterdam, Netherlands/33e</p>
+        <h5 className="text-brand">SHOP HOURS:</h5>
+        <p className="text-white">
+          Monday - Friday: 9:00am - 5:00pm, Saturday: 9:00am - 12:00am
+        </p>
+        <p className="text-white">
+          Sunday: <span style={{ color: "red" }}>CLOSED</span>
+        </p>
+        <h3 className="text-white">
+          <FontAwesomeIcon icon={faPhoneAlt} style={{ color: "#319ce3" }} /> 333
+          222 999
+        </h3>
+        <a
+          target="_blank"
+          href="https://www.facebook.com"
+          rel="noreferrer"
+          className="p-2 fs-3"
+        >
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.twitter.com"
+          rel="noreferrer"
+          className="p-2 fs-3"
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com"
+          rel="noreferrer"
+          className="p-2 fs-3"
+        >
+          <FontAwesomeIcon icon={faInstagramSquare} />
+        </a>
+      </div>
+      <div className="copyright text-center p-3">
+        <h6 className="text-white">© 2021 ALL RIGHTS RESERVED</h6>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
