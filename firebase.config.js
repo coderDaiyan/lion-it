27d85ef46af17ae619f61f683566a0a330666fd7
coderDaiyan <daiyanabdallah@gmail.com>
2021-05-30 21:21:19 +0600

    review problem solved

--- firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBxLi9S3LYq9Uc6Oi6Ey_PI9cnP14Dq1kI",
-  authDomain: "lion-it.firebaseapp.com",
-  projectId: "lion-it",
-  storageBucket: "lion-it.appspot.com",
-  messagingSenderId: "684178710099",
-  appId: "1:684178710099:web:19000b26fa8a2f03ee30a6",
-};
--- src/components/Home/Review/Review.js
+++ src/components/Home/Review/Review.js
@@ -2,8 +2,28 @@ import React from "react";
 import { useSelector } from "react-redux";
 import ReviewCard from "../ReviewCard/ReviewCard";
 
+const reviewData = [
+  {
+    name: "Khairul Islam",
+    notes:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore unde esse corrupti voluptate commodi explicabo?",
+  },
+  {
+    name: "Walter White",
+    notes:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore unde esse corrupti voluptate commodi explicabo?",
+  },
+  {
+    name: "Walter White",
+    notes:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore unde esse corrupti voluptate commodi explicabo?",
+  },
+];
+
 const Review = () => {
   const state = useSelector((state) => state.review.reviews.slice(0, 3));
+
+  console.log(state);
 
   return (
     <section className="mt-5 review_card">
@@ -12,9 +32,13 @@ const Review = () => {
         <h1 className="text-uppercase">About Us</h1>
       </div>
       <div className="row mb-5 w-100 p-5">
-        {state.map((review) => (
-          <ReviewCard key={review.id} review={review}></ReviewCard>
-        ))}
+        {state.length > 0
+          ? state.map((review) => (
+              <ReviewCard key={review.id} review={review}></ReviewCard>
+            ))
+          : reviewData.map((review) => (
+              <ReviewCard key={review.id} review={review}></ReviewCard>
+            ))}
       </div>
     </section>
   );
